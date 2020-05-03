1115c8be980666dd859d55600b80b81d1bf16853
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-03 22:56:08 +0900

    APIコントローラ作成

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,11 @@
+class ApiController < ActionController::API
+  def index
+    tweets = Tweet.all
+    render json: tweets
+  end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
+    render json: @tweet
+  end
+end
