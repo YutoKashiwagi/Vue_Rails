ea991150f6b5a0f50a937141794696532966c191
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-04 01:35:29 +0900

    アクション定義場所間違いを修正

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -1,2 +1,11 @@
 class Api::V1::TweetsController < ApiController
+  def index
+    tweets = Tweet.all
+    render json: tweets
+  end
+  
+  def show
+    @tweet = Tweet.find(params[:id])
+    render json: @tweet
+  end
 end
