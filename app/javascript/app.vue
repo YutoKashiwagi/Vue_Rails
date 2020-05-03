89ec10c161a0407f1a25072fe0189277125498f2
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-04 01:26:05 +0900

    vueでtweet一覧を表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,15 +1,33 @@
 <template>
   <div id="app">
-    <p>{{ message }}</p>
+    <table>
+      <tbody>
+        <tr>
+          <th>ID</th>
+          <th>Content</th>
+        </tr>
+        <tr v-for="tweet in tweets" :key="tweet.id">
+          <td>{{ tweet.id }}</td>
+          <td>{{ tweet.content }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      tweets: []
     }
+  },
+  mounted () {
+    axios
+      .get('/api/v1/tweets.json')
+      .then(response => (this.tweets = response.data))
   }
 }
 </script>
