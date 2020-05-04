7aac6e6b3f129f24ccd4c738ffd50ad411157b6c
YutoKashiwagi <kashiwagi19950714@gmail.com>
2020-05-04 20:10:49 +0900

    一覧、詳細ページを実装

---
+++ app/javascript/TweetDetail.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>ID: {{ tweet.id }}</p>
+    <p>Content: {{ tweet.content }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function () {
+    return {
+      tweet: {}
+    }
+  },
+  mounted () {
+    axios
+      .get(`/api/v1/tweets/${this.$route.params.id}.json`)
+      .then(response => (this.tweet = response.data))
+  }
+}
+</script>
+<style scoped>
+</style>
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,40 +1,37 @@
 <template>
-  <div id="app">
-    <table>
-      <tbody>
-        <tr>
-          <th>ID</th>
-          <th>Content</th>
-        </tr>
-        <tr v-for="tweet in tweets" :key="tweet.id">
-          <td>{{ tweet.id }}</td>
-          <td>{{ tweet.content }}</td>
-        </tr>
-      </tbody>
-    </table>
+  <div>
+    <router-link to="/">Index</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import TweetsIndexVue from 'TweetsIndex.vue'
+import TweetDetailVue from './TweetDetail.vue'
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/',
+      component: TweetsIndexVue
+    },
+    {
+      path: '/tweets/:id(\\d+)',
+      name: 'TweetDetail',
+      component: TweetDetailVue
+    }
+  ]
+})
+
+Vue.use(VueRouter)
 
 export default {
-  data: function () {
-    return {
-      tweets: []
-    }
-  },
-  mounted () {
-    axios
-      .get('/api/v1/tweets.json')
-      .then(response => (this.tweets = response.data))
-  }
+  router
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
 </style>
